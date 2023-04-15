964adad0e02796087e5f5823c8f50917afb2aee6
Yurii <ranoveria@gmail.com>
2023-04-15 13:06:44 +0200

    learn(sessions and auth): encrypt password

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const bcrypt = require('bcryptjs');
 const User = require('../models/user');
 const router = Router();
 
@@ -21,7 +22,7 @@ router.post('/login', async (req, res) => {
 
     const candidate = await User.findOne({ email });
     if (candidate) {
-      const passwordsMatch = password === candidate.password;
+      const passwordsMatch = await bcrypt.compare(password, candidate.password);
 
       if (passwordsMatch) {
         req.session.user = candidate;
@@ -48,6 +49,7 @@ router.post('/login', async (req, res) => {
 
 router.post('/signup', async (req, res) => {
   try {
+    // ToDo: validate password == confirm
     const { email, password, confirm, name } = req.body;
 
     const candidate = await User.findOne({ email });
@@ -55,10 +57,11 @@ router.post('/signup', async (req, res) => {
     if (candidate) {
       res.redirect('/auth/login#register');
     } else {
+      const hashPassword = await bcrypt.hash(password, 10);
       const user = new User({
         email,
         name,
-        password,
+        password: hashPassword,
         cart: { items: [] },
       });
       await user.save();
